f32735371d024a6653ca20854b0bebe4cadced2c
mohamed99akram <mohamed1999akram@gmail.com>
2022-10-19 14:41:43 +0200

    CRUD done for json file

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,9 @@
 
-from django.urls import path, include
-from .views import index
+from django.urls import path
+from .views import index, create, update, remove
 urlpatterns = [
     path("", index),
+    path("add/", create),
+    path("change/", update),
+    path("remove/", remove)
 ]
--- student/views.py
+++ student/views.py
@@ -1,6 +1,67 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
 
+import json
+
+def readFile() -> list:
+    f = open('student/student.json', 'r')
+    data = json.loads(f.read())
+    f.close()
+    return data
 # Create your views here.
-
+# GET Request
 def index(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    if(request.method=='GET'):
+        data = readFile()
+        return JsonResponse(data, safe=False)
+    else:
+        return HttpResponse("Invalid Request")
+
+# POST Request
+def create(request):
+    if(request.method=='POST'):
+        data = readFile()
+        data.append(json.loads(request.body))
+        with open('student/student.json', 'w') as f:
+            f.write(json.dumps(data))
+        return JsonResponse(data, safe=False)
+    else:
+        return HttpResponse("Invalid Request")
+
+# PUT Request
+def update(request):
+    if(request.method=='PUT'):
+        newData = json.loads(request.body)
+        data = readFile()
+        index = -1
+        for i in range(len(data)):
+            if(data[i]['id']==newData['id']):
+                index = i
+                break
+        if(index!=-1):
+            data[index] = newData
+        else:
+            data.append(newData)
+        with open('student/student.json', 'w') as f:
+            f.write(json.dumps(data))
+        return JsonResponse(data, safe=False)
+    else:
+        return HttpResponse('Invalid Request')
+
+# DELETE Request
+def remove(request):
+    if(request.method=='DELETE'):
+        data = readFile()
+        id = json.loads(request.body)['id']
+        index = -1
+        for i in range(len(data)):
+            if data[i]['id']==id:
+                index = i
+                break
+        if(index!=-1):
+            data.pop(index)
+            with open('student/student.json', 'w') as f:
+                f.write(json.dumps(data))
+        return JsonResponse(data, safe=False)
+    else:
+        return HttpResponse('Invalid Request')
